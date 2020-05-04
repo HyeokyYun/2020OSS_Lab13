38196cb493d8d17e4627b4ebc40c975b427bd3c1
Hyeok Yun <hyoeky98@gmail.com>
2020-05-04 19:17:05 +0900

    score.c

--- score.c
+++ score.c
@@ -4,11 +4,11 @@ int addScore(Score *s){
 	printf("학생의 이름: ");
 	scanf("%s\n", s->name);
 	printf("국어점수: ");
-	scanf("%d\n", &p->kor);
+	scanf("%d\n", &s->kor);
 	printf("수학점수: ");
-	scanf("%d\n", &p->math);
+	scanf("%d\n", &s->math);
 	printf("영어점수: ");
-	scanf("%d\n", &p->eng);
+	scanf("%d\n", &s->eng);
 	printf("==> 추가됨!\n");
 	
 	return 1;
@@ -16,8 +16,31 @@ int addScore(Score *s){
 
 void readScore(Score s){
 	if(s.kor == -1 && s.math == -1 && s.eng == -1) return;
-	p.avg = p.sum/3.0;
+	s.sum = s.kor + s.math + s.eng;
+	s.avg = s.sum/3.0;
 	printf("%s %d %d %d %d %.1f", s.name, s.kor, s.math, s.eng, s.sum, s.avg);
+}
+
+int updateScore(Score *s){
+	printf("학생의 이름: ");
+	scanf("%s\n", s->name);
+	printf("국어점수: ");
+	scanf("%d\n", &s->kor);
+	printf("수학점수: ");
+	scanf("%d\n", &s->math);
+	printf("영어점수: ");
+	scanf("%d\n", &s->eng);
+	printf("==> 수정됨!\n");
+	
+	return 1;
+}
+
+int deleteScore(Score *s){
+	s->kor = -1;
+	s->math = -1;
+	s->eng = -1;
+	printf("==> 삭제됨!\n");
+	return 1;
 }
 
 
