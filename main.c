628098fe4d8dad830dcc70270b8704b8c3b929d5
Hyeok Yun <hyoeky98@gmail.com>
2020-05-04 19:24:11 +0900

    main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "total.h"
+
+int Menu(){
+	int num;
+	printf("====메뉴목록====\n");
+	printf("1. 조회\n");
+	printf("2. 추가\n");
+	printf("3. 수정\n");	
+	printf("4. 삭제\n");
+	printf("0. 종료\n");
+	printf("원하는 작업: ");
+	scanf("%d", &num);
+	
+	return num;
+}
+
+int main(void){
+	Score s;
+	int choice;
+	
+	while(1){
+		choice = Menu():
+		
+	}
+}
